4c8bbd0a262b4ba946cd5a3dd5235c7df037ad93
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2024-02-29 19:22:10 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.80 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String str1 = String.valueOf(a);
+        String str2 = String.valueOf(b);
+        
+        
+        int aa = Integer.valueOf(str1+str2).intValue();
+        int bb = Integer.valueOf(str2+str1).intValue();
+        
+        if( aa > bb) {
+            answer = aa;
+            
+        }else if(aa == bb) {
+            answer = aa;
+        }else {
+            answer = bb;
+        }
+        
+        return answer;
+    }
+}
