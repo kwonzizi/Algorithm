2ac75f9a7106625b6e3772d73016a5a28e32d577
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2024-03-02 15:26:29 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int a = 1;
+        int b = 0;
+        for(int i = 0; i<num_list.length; i++) {
+            a *= num_list[i];
+            b += num_list[i];
+        }
+        b = b*b;
+        answer = (a < b)? 1 : 0;
+        return answer;
+    }
+}
