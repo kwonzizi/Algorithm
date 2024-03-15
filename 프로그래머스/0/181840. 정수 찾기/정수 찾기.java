fc4afb808dae9f63c31dc8f265078352f224f468
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2024-03-15 16:33:55 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,11 +1,17 @@
 class Solution {
     public int solution(int[] num_list, int n) {
         int answer = 0;
-        for(int i = 0; i< num_list.length; i++) {
-            if(num_list[i] == n) {
-                answer = 1;
-                break;
-            }else answer = 0;
+        // for(int i = 0; i< num_list.length; i++) {
+        //     if(num_list[i] == n) {
+        //         answer = 1;
+        //         break;
+        //     }else answer = 0;
+        // }
+        
+        for (int x : num_list) {
+            if(x == n) {
+                return 1;
+            }
         }
      
         return answer;
