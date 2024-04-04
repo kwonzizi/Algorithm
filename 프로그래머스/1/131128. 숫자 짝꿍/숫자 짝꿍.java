711f7096a2fda3621077332f1edc6a4e240ba9ca
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2024-04-04 14:29:46 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    static public String solution(String X, String Y) {
+        StringBuilder answer = new StringBuilder();
+
+        int[] x = new int[10];
+        int[] y = new int[10];
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < X.length(); i++) {
+            int temp = X.charAt(i) - '0';
+            x[temp]++;
+           
+        }
+        for (int i = 0; i < Y.length(); i++) {
+            int temp = Y.charAt(i) - '0';
+            y[temp]++;
+         
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            
+            if (x[i] != 0 && y[i] != 0) {
+                
+                for (int j = 0; j < Math.min(x[i], y[i]); j++) list.add(i);
+            }
+        }
+
+        if (list.size() == 0) {
+            return "-1";
+        } else if (list.get(0) == 0) {
+            return "0";
+        }
+
+        for (int i : list) {
+            answer.append(i);
+        }
+
+        return answer.toString();
+    }
+
+}
