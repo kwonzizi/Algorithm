76e7abf374f9def4fc11a861a15e681e5a31975e
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2024-04-04 15:51:12 +0900

    [level 1] Title: 평균 구하기, Time: 0.07 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+
+        for(int a : arr) answer += a;
+  
+        return answer/arr.length;
+    }
+}
