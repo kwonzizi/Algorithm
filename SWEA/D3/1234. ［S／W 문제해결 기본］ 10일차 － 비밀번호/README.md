0b0b4db055615756c3485090554c7b6833bf6627
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2024-05-04 18:59:31 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 191 ms, Memory: 26,968 KB -BaekjoonHub

---
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/README.md
@@ -0,0 +1,15 @@
+# [D3] [S/W 문제해결 기본] 10일차 - 비밀번호 - 1234 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14_DEKAJcCFAYD) 
+
+### 성능 요약
+
+메모리: 26,968 KB, 시간: 191 ms, 코드길이: 1,253 Bytes
+
+### 제출 일자
+
+2024-05-04 18:59
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+	
+		Scanner sc = new Scanner(System.in);
+	
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+		
+			int T=sc.nextInt();
+            String str = sc.next();
+            
+            char[] arr = str.toCharArray();
+            List<Integer> list = new ArrayList<>();
+        	StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < T; i++) {   
+                int a= arr[i]-'0';
+                list.add(a);
+              }
+            int j = 0;
+            int cnt = 0;
+          
+            while(j < list.size()-1) {
+                int y = 0;
+              if(j>=0 && list.get(j) == list.get(j+1)) {
+                  while(y<2){
+                      list.remove(j);  
+                      y++;
+                  }
+           		   j--;
+              }else {
+                 
+                   j++;
+                
+             }
+                 
+            }
+           
+            for(int i = 0; i < list.size(); i++) {
+       		      sb.append(list.get(i));
+            }
+            
+          System.out.println( "#" + test_case + " " +sb);
+  			
+
+		}
+	}
+}
