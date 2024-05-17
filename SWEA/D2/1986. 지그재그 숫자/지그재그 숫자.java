7ae9258f6c34c2a30e1daad80c57bc4ebac30ef3
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2024-05-17 15:34:14 +0900

    [D2] Title: 지그재그 숫자, Time: 135 ms, Memory: 26,332 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+	
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            int N = sc.nextInt();
+             int sum = 0;
+            for(int i = 1; i <= N; i++) {
+                
+                if(i % 2 == 1) { // 1 2
+           		     sum += i;
+                }else {
+                    sum -=i;
+                }
+            }
+           
+           
+            System.out.println("#" + test_case + " " + sum);
+
+		}
+	}
+}
